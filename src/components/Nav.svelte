8f876adc8820199b93d88d74c5d98ecdfd6d25c8
lfbergee <leiv.fredrik.berge@elate.no>
2020-01-03 14:43:53 +0100

    feat: add graphcms as blog backend

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -50,10 +50,8 @@
 
 <nav>
 	<ul>
-		<li><a class:selected='{segment === undefined}' href='.'>chevron button</a></li>
+		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen 
-		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>-->
+		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
 	</ul>
 </nav>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,34 +1,55 @@
 <script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
+  export async function preload(page, session) {
+    const res = await this.fetch("https://api-euwest.graphcms.com/v1/ck4y6untahrrr01bqdwyr6z6h/master", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `{
+          posts: blogs(where: {
+            status: PUBLISHED
+          }) {
+            slug
+            title
+            description
+          }
+        }`
+      })
+    });
+    const json = await res.json();
+
+    if (res.status === 200) {
+      return json.data;
+    } else {
+      this.error(res.status, json && json.errors);
+    }
+  }
 </script>
 
 <script>
-	export let posts;
+  export let posts;
 </script>
 
 <style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
+  ul {
+    margin: 0 0 1em 0;
+    line-height: 1.5;
+  }
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+  <title>Blog</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
+<h1>Blog posts</h1>
 
 <ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
-	{/each}
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
 </ul>
