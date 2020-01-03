18b11cdd766408fa361cb5678972ce1e9ad9d47f
lfbergee <leiv.fredrik.berge@elate.no>
2020-01-03 16:55:46 +0100

    feat: refactor chevronbutton

---
+++ src/components/ChevronButton.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<style>
+  button {
+    border: 1px solid #3d3d3d;
+    border-radius: 4px;
+    background-color: #fafafa;
+    color: #3d3d3d;
+    position: relative;
+    padding: 12px 38px 12px 12px;
+    font-size: 1rem;
+    transition: 150ms cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition-property: background-color, color;
+  }
+
+  button::after {
+    content: "›";
+    font-size: 2rem;
+    line-height: 0.9rem;
+    position: absolute;
+    right: 18px;
+    transition: 250ms cubic-bezier(0.075, 0.82, 0.165, 1) right;
+  }
+
+  button:hover::after {
+    right: 16px;
+  }
+
+  button:active {
+    background-color: #3d3d3d;
+    color: #fafafa;
+  }
+
+  button:active::after {
+    right: 8px;
+  }
+</style>
+
+<button on:click={() => dispatch('click')}>
+  <slot />
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,17 @@
-<style>
-	button {
-		border: 1px solid #3d3d3d;
-		border-radius: 4px;
-		background-color: #fafafa;
-		color: #3d3d3d;
-		position: relative;
-		padding: 12px 32px 12px 12px;
-		font-size: 1rem;
-		transition: 150ms cubic-bezier(0.075, 0.82, 0.165, 1);
-		transition-property: background-color, color;
+<script>
+  import ChevronButton from "../components/ChevronButton.svelte";
 
-	}
-
-	button::after {
-		content: ">";
-		position: absolute;
-		right: 12px;
-		transition: 250ms cubic-bezier(0.075, 0.82, 0.165, 1) right;
-	}
-
-	button:hover::after {
-		right: 8px;
-	}
-
-	button:active {
-		background-color: #3d3d3d;
-		color: #fafafa;
-	}
-
-</style>
+  function handleClick(event) {
+    alert("You clicked the chevron button, it's probably quite happy now 😎");
+  }
+</script>
 
 <svelte:head>
-	<title>Chevron button</title>
+  <title>Chevron button</title>
 </svelte:head>
 
 <h1>The home of the humble chevron button</h1>
-<button>Chevron button</button>
-
+<section>
+  <h2>Svelte ChevronButton</h2>
+  <ChevronButton on:click={handleClick}>The chevron button</ChevronButton>
+</section>
